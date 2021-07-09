66fbafad0483157a7899f4901e7d44e252131685
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-07-09 17:44:40 -0300

    Requisito 10 - Completo

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-let input = document.querySelector('#texto-tarefa');
-let addButton = document.querySelector('#criar-tarefa');
-let taskList = document.querySelector('#lista-tarefas');
-let task = document.getElementsByTagName('li');
+const input = document.querySelector('#texto-tarefa');
+const addButton = document.querySelector('#criar-tarefa');
+const taskList = document.querySelector('#lista-tarefas');
+const task = document.getElementsByTagName('li');
 let currentTask = document.getElementsByClassName('selected');
+const clearButton = document.querySelector('#apaga-tudo');
+const header = document.querySelector('header');
 
 /* -----Requisito 5 e 6----- */
 function createTask() {
-  let listItem = document.createElement('li');
-  listItem.innerHTML = input.value
+  const listItem = document.createElement('li');
+  listItem.innerHTML = input.value;
   taskList.appendChild(listItem);
   input.value = '';
 }
 
-addButton.addEventListener('click', createTask)
+addButton.addEventListener('click', createTask);
+
 /* -----Requisito 7 e 8----- */
 function colorTask(clickedItem) {
   const selectedTask = clickedItem.target;
   if (selectedTask !== currentTask) {
-    for (let index = 0; index < task.length; index++) {
-      task[index].classList.remove('selected')
+    for (let index = 0; index < task.length; index += 1) {
+      task[index].classList.remove('selected');
     }
     selectedTask.classList.add('selected');
   }
-  currentTask = selectedTask
+  currentTask = selectedTask;
 }
 
-taskList.addEventListener('click', colorTask)
+taskList.addEventListener('click', colorTask);
 
+/* -----Requisito 10----- */
+function clear() {
+  header.removeChild(taskList);
+  const createOl = document.createElement('ol');
+  createOl.id = 'lista-tarefas';
+}
+clearButton.addEventListener('click', clear);
