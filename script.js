d5cd396b79a7c6121564a4f02628c06df3ec2f27
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-07-09 15:11:07 -0300

    Requisito #7 - Completo

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const addButton = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 const task = document.querySelector('.task');
 
-/* -----Requisito 5----- */
+/* -----Requisito 5 e 6----- */
 function createTask() {
   let listItem = document.createElement('li');
   listItem.className = 'task';
@@ -13,3 +13,9 @@ function createTask() {
 }
 
 addButton.addEventListener('click', createTask)
+/* -----Requisito 7----- */
+function colorTask(clickedItem) {
+  clickedItem.target.setAttribute('style', 'background-color: rgb(128, 128, 128)')
+}
+
+taskList.addEventListener('click', colorTask)
